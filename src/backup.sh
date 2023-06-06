33f693c400c404b1ac611964ef8e511fec7d03ca
dspacheco <diogopacheco132@gmail.com>
2023-06-06 11:11:55 +0000

    update

--- src/backup.sh
+++ src/backup.sh
@@ -6,4 +6,4 @@ base_name=backup.sql
 current_time=$(date +"%Y-%m-%d_%H:%M:%S")
 file_name=$curr_time_$base_name
 
-mysqldump -u root -p$MYSQL_ROOT_PASSWORD mysql > $file_name.backup.sql 2>/dev/null
+mysqldump -u root -p$MYSQL_ROOT_PASSWORD mysql > $file_name 2>/dev/null
